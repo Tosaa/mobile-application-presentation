1364b3d865e8296bf88ebbcd1e0a3efa08d1755d
Toni <a.saatze@googlemail.com>
2020-05-06 15:00:16 +0200

    add random T-Shirt button

--- code/app/src/main/java/com/example/livedatapresentation/MainActivity.kt
+++ code/app/src/main/java/com/example/livedatapresentation/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.livedatapresentation
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 
@@ -14,9 +17,23 @@ class MainActivity : AppCompatActivity() {
 
         Timber.plant(DebugTree())
         Timber.d("onCreate: ${this::class.java}")
-        val model = ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory(application)).get(TShirtSelectorViewModel::class.java)
+        val model =
+            ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(application)).get(
+                TShirtSelectorViewModel::class.java
+            )
         setContentView(R.layout.activity_main)
+        button_detail.setOnClickListener {
+            Timber.d("toggle : DetailFragment (not implemented yet)")
+        }
+        button_overview.setOnClickListener {
+            Timber.d("toggle : OverviewFragment (not implemented yet)")
+        }
+        button_add_random.setOnClickListener {
+            Timber.d("click : add random T-Shirt to ViewModel (not implemented yet)")
+        }
+
     }
+
 
     override fun onPause() {
         super.onPause()
